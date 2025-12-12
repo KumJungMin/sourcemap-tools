6cbf7be24cf329cda2d2cbed6237b0615a01a50a
KumJungMin <rmawjdals@naver.com>
2025-12-12 14:42:41 +0900

    feat: add sample test vue app

---
+++ apps/vue-test-app/src/App.vue
@@ -0,0 +1,9 @@
+<template><div><h1>Sourcemap Test</h1><ExampleComponent/></div></template>
+
+<script setup lang='ts'>
+import { defineAsyncComponent } from 'vue';
+
+const ExampleComponent = defineAsyncComponent(() => 
+  import('./components/ExampleComponent.vue')
+);
+</script> 
---
+++ apps/vue-test-app/src/components/ExampleComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <button @click="boom">Throw Error</button>
+</template>
+<script setup lang='ts'>
+function boom(){
+  throw new Error("Test error");
+}
+</script>
